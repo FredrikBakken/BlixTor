16cf25c32da99ef011ebb3419a4e42ce3be124fc
Fredrik Bakken <fredda10x@gmail.com>
2016-10-15 23:43:45 +0200

    Formatting
    Formatting source code.

--- src/BlixTorClient.java
+++ src/BlixTorClient.java
@@ -1,9 +1,9 @@
 import javafx.application.Application;
 
 public abstract class BlixTorClient extends Application {
-	
+
 	public static void main(String[] args) {
 		Application.launch(BlixTorGUI.class, args);
 	}
-	
+
 }
--- src/BlixTorDownload.java
+++ src/BlixTorDownload.java
@@ -9,31 +9,26 @@ import com.turn.ttorrent.client.SharedTorrent;
 
 public class BlixTorDownload {
 	private static final double noUploadRate = Double.MIN_VALUE;
-	
+
 	public static String browseTorrentFile = "";
 	public static String downloadLocation = "";
-	
+
 	public static double downloadRate = 0;
 	public static double uploadRate = 0;
 	public static int seedTime = 0;
-	
-	public static void DownloadTorrent() throws UnknownHostException, IOException, NoSuchAlgorithmException {
-		Client client = new Client(
-				InetAddress.getLocalHost(),
 
-				SharedTorrent.fromFile(
-						new File(browseTorrentFile),
-						new File(downloadLocation)
-				)
-		);
-		
+	public static void DownloadTorrent() throws UnknownHostException, IOException, NoSuchAlgorithmException {
+		Client client = new Client(InetAddress.getLocalHost(),
+
+				SharedTorrent.fromFile(new File(browseTorrentFile), new File(downloadLocation)));
+
 		client.setMaxDownloadRate(downloadRate);
 		client.setMaxUploadRate(noUploadRate);
-		
+
 		client.download();
-		
+
 		client.share(seedTime);
-		
+
 		client.waitForCompletion();
 	}
 }
